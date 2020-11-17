0c19a5edd970da99a05acfd45c22296ec90ab2dc
iamfiasco <iamfiascoiv@gmail.com>
2020-11-17 19:57:49 +0530

    adding user model

--- models.js
+++ models.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 mongoose.connect("mongodb://localhost", {useNewUrlParser: true})
 
 const songSchema = new mongoose.Schema({
-	fpath: String,
+	fpath: {
+				type: String,
+				required: true
+			},
 	title: String,
 	album: String,
 	genre: String,
@@ -19,10 +22,31 @@ const librarySchema = new mongoose.Schema({
 	description: String,
 })
 
+const userSchema = new mongoose.schema({
+	username: {
+					type: String,
+					required: true
+			},
+	age: Number,
+	createdAt: {
+					type: Date,
+					default: Date.now()
+			},
+	password: {
+					type: String,
+					required: true
+			}
+})
+
 const songsModel = new mongoose.Model("musu", songSchema)
 const libraryModel = new mongoose.Model("musu", librarySchema)
+const userModel = new mongoose.Model("musuA", userSchema)
 
 module.exports = {
 	songsModel: songsModel,
 	librarySchema: librarySchema
+	userModel: userModel
 }
+
+
+
