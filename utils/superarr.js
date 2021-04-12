fd0b2120ade09bacd82024cfef7d2d1fad26afa2
Nikhil Surya Mukhi <iamfiascoiv@gmail.com>
2021-04-12 06:28:16 +0530

    started revamp

--- utils/superarr.js
+++ utils/superarr.js
@@ -1,9 +1,12 @@
 module.exports = {
   init: function(){
+    
+    //returns last element of the list
     if(Array.prototype.last === undefined){
       Array.prototype.last = function(){
         return this[this.length - 1]
       }
     }
+  
   }
 }
